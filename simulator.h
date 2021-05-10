52c7d77afefdd2e2ad6c67a4d00e41da959bf17e
Ozan Sazak <ozan.sazak@protonmail.ch>
2021-05-10 18:49:25 +0300

    updated simulator.h

--- simulator.h
+++ simulator.h
@@ -1,10 +1,15 @@
 #ifndef __CENG_SIMULATOR_H
 #define __CENG_SIMULATOR_H
 
+#include <time.h>
+#include <stdlib.h>
 #include <pthread.h>
 #include <semaphore.h>
 
 #include "config_parser.h"
+
+#include "writeOutput.h"
+#include "helper.h"
 
 #define DEBUG_SIMULATOR 1
 
