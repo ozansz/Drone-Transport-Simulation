93fa593b204095b622598471f4697543fc2e8151
Ozan Sazak <ozan.sazak@protonmail.ch>
2021-05-11 14:52:17 +0300

    sender impl

--- simulator.c
+++ simulator.c
@@ -13,15 +13,17 @@ int total_hubs_count;
 int* hub_activity_registry = NULL; // int used as bo0l
 
 pthread_mutex_t drone_info_mutex = PTHREAD_MUTEX_INITIALIZER;
-pthread_mutex_t sender_info_mutex = PTHREAD_MUTEX_INITIALIZER;
 pthread_mutex_t receiver_info_mutex = PTHREAD_MUTEX_INITIALIZER;
+
+pthread_mutex_t sender_info_mutex = PTHREAD_MUTEX_INITIALIZER;
+int* sender_activity_registry = NULL; // int used as bo0l
 
 pthread_mutex_t* hub_incoming_storage_mutexes = NULL;
 pthread_mutex_t* hub_outgoing_storage_mutexes = NULL;
 pthread_mutex_t* hub_charging_spaces_mutexes = NULL;
 int* incoming_storage_remaining = NULL;
 PackageInfo*** incoming_storages = NULL; 
-int* outgoing_storage_remaining = NULL;
+int* outgoing_storage_remaining = NULL; // hub look at (outgoing_storage_remaining < self.outgoing_capacity)
 PackageInfo*** outgoing_storages = NULL; 
 int* charging_spaces_remaining = NULL;
 
@@ -30,6 +32,7 @@ pthread_mutex_t debug_printf_mutex = PTHREAD_MUTEX_INITIALIZER;
 
 #define DEBUG_MUTEX_LOCK if (DEBUG_SIMULATOR) { pthread_mutex_lock(&debug_printf_mutex); }
 #define DEBUG_MUTEX_RELEASE if (DEBUG_SIMULATOR) { pthread_mutex_unlock(&debug_printf_mutex); }
+#define DEBUG_LOG_SAFE(x) if (DEBUG_SIMULATOR) { pthread_mutex_lock(&debug_printf_mutex); x; pthread_mutex_unlock(&debug_printf_mutex); }
 
 void defer(void) {
     if (DEBUG_SIMULATOR)
@@ -70,6 +73,9 @@ void defer(void) {
     
     if (hub_activity_registry != NULL)
         free(hub_activity_registry);
+
+    if (sender_activity_registry != NULL)
+        free(sender_activity_registry);
 
     // TODO: Free "incoming_storages"
     // TODO: Free "outgoing_storages"
@@ -138,6 +144,13 @@ void init_critical_structures(SimulationConfig* conf) {
         exit(1);
     }
 
+    sender_activity_registry = (int*) malloc(sizeof(int) * conf->hubs_count);
+
+    if (sender_activity_registry == NULL) {
+        perror("malloc/sender_activity_registry");
+        exit(1);
+    }
+
     incoming_storages = (PackageInfo***) malloc(sizeof(PackageInfo**) * conf->hubs_count);
 
     if (incoming_storages == NULL) {
@@ -157,6 +170,7 @@ void init_critical_structures(SimulationConfig* conf) {
         outgoing_storage_remaining[i] = conf->hubs[i].outgoing_storge_size;
         charging_spaces_remaining[i] = conf->hubs[i].charging_space_count;
         hub_activity_registry[i] = 1;
+        sender_activity_registry[i] = 1;
 
         incoming_storages[i] = (PackageInfo**) malloc(sizeof(PackageInfo*) * conf->hubs[i].incoming_storge_size);
 
@@ -165,30 +179,42 @@ void init_critical_structures(SimulationConfig* conf) {
             exit(1);
         }
 
+        for (int j = 0; j < conf->hubs[i].incoming_storge_size; j++)
+            incoming_storages[i][j] = NULL;
+
         outgoing_storages[i] = (PackageInfo**) malloc(sizeof(PackageInfo*) * conf->hubs[i].outgoing_storge_size);
     
         if (outgoing_storages[i] == NULL) {
             perror("malloc/outgoing_storages[i]");
             exit(1);
         }
+
+        for (int j = 0; j < conf->hubs[i].outgoing_storge_size; j++)
+            outgoing_storages[i][j] = NULL;
     }
 }
 
-void sender_thread(void* sender_info, void *sim_config) {
+void sender_thread(void* sender_info, void *sim_config, void* self_conf) {
+    PackageInfo* new_package = NULL;
     SenderInfo* self = (SenderInfo*) sender_info;
+    SenderConfig* self_config = (SenderConfig*) self_conf;
     SimulationConfig* simulation_config = (SimulationConfig*) sim_config;
 
-    if (DEBUG_SIMULATOR) {
-        pthread_mutex_lock(&debug_printf_mutex);
-        printf("Sender Thread awake (id: %d, hub: %d, packages: %d)\n", self->id, self->current_hub_id, self->remaining_package_count);
-        pthread_mutex_unlock(&debug_printf_mutex);
-    }    
+    DEBUG_LOG_SAFE(printf("Sender Thread awake (id: %d, hub: %d, packages: %d)\n", self->id, self->current_hub_id, self->remaining_package_count))
 
-    DEBUG_MUTEX_LOCK
-    WriteOutput(self, NULL, NULL, NULL, SENDER_CREATED);
-    DEBUG_MUTEX_RELEASE
+    // if (DEBUG_SIMULATOR) {
+    //     pthread_mutex_lock(&debug_printf_mutex);
+    //     printf("Sender Thread awake (id: %d, hub: %d, packages: %d)\n", self->id, self->current_hub_id, self->remaining_package_count);
+    //     pthread_mutex_unlock(&debug_printf_mutex);
+    // }    
 
-    while (self->remaining_package_count > 0) {
+    DEBUG_LOG_SAFE(WriteOutput(self, NULL, NULL, NULL, SENDER_CREATED))
+
+    // DEBUG_MUTEX_LOCK
+    // WriteOutput(self, NULL, NULL, NULL, SENDER_CREATED);
+    // DEBUG_MUTEX_RELEASE
+
+    for (int _packet_index = 0; self->remaining_package_count > 0; _packet_index++, self->remaining_package_count--) {
         int active_hubs_count = 0;
         pthread_mutex_lock(&hub_info_mutex);
 
@@ -218,8 +244,56 @@ void sender_thread(void* sender_info, void *sim_config) {
             exit(1);
         }
 
-        // TODO: Here
+        int receiver_id = simulation_config->hubs[hub_id-1].receiver.receiver_id;
+
+        DEBUG_LOG_SAFE(printf("Sender Thread %d: Selected Hub-%d:Rec-%d for package #%d\n", self->id, hub_id, receiver_id, _packet_index))
+        
+        if (new_package != NULL)
+            free(new_package);
+
+        new_package = (PackageInfo*) malloc(sizeof(PackageInfo));
+
+        if (new_package == NULL) {
+            perror("malloc/new_package");
+            exit(1);
+        }
+
+        FillPacketInfo(new_package, self->id, self->current_hub_id, receiver_id, hub_id);
+        FillSenderInfo(self, self->id, self->current_hub_id, self->remaining_package_count, new_package);
+
+        // OLD TODO:
+        // put package to self's hub's array on "outgoing_storages"
+        while (1) {
+            pthread_mutex_lock(&hub_outgoing_storage_mutexes[hub_id-1]); // WaitCanDeposit
+            
+            if (outgoing_storage_remaining[hub_id-1] > 0) {
+                int new_package_index_in_store = simulation_config->hubs[hub_id-1].outgoing_storge_size - outgoing_storage_remaining[hub_id-1];
+                
+                outgoing_storages[hub_id-1][new_package_index_in_store] = new_package; // SenderDeposit
+                outgoing_storage_remaining[hub_id-1] -= 1;
+                
+                pthread_mutex_unlock(&hub_outgoing_storage_mutexes[hub_id-1]);
+                
+                break;
+            }
+
+            pthread_mutex_unlock(&hub_outgoing_storage_mutexes[hub_id-1]);
+        }
+
+        // FillPacketInfo && FillSenderInfo && WriteOutput
+        DEBUG_LOG_SAFE(WriteOutput(self, NULL, NULL, NULL, SENDER_DEPOSITED))
+
+        // sleep()
+        // TODO: Change this to: wait()
+        _wait(self_config->wait_time_between_packages);
     }
+
+    pthread_mutex_lock(&sender_info_mutex);
+    sender_activity_registry[self->id-1] = 0;
+    pthread_mutex_unlock(&sender_info_mutex);
+
+    FillSenderInfo(self, self->id, self->current_hub_id, self->remaining_package_count, NULL);
+    DEBUG_LOG_SAFE(WriteOutput(self, NULL, NULL, NULL, SENDER_STOPPED))
 }
 
 int main(int argc, char **argv, char **envp) {
